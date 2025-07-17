5ed7910c3f208f1427e8dc68969498e998358c40
Daniel Brotsky <dan@brotsky.com>
2025-07-17 00:12:59 -0700

    Always eject the drive after backing up.
    Fixes #1.

--- tm-backup-and-eject.sh
+++ tm-backup-and-eject.sh
@@ -3,9 +3,9 @@ hostname="$(scutil --get ComputerName)"
 drive="$hostname backup"
 date
 if mount | grep "$drive" ; then
-	echo "Volume $drive mounted, starting backup..."
-	# eject after successful backup, stay mounted otherwise
-	tmutil startbackup -b && diskutil unmount "$drive"
+	echo "Volume '$drive' mounted, starting backup..."
+	tmutil startbackup -b
+	diskutil unmount "$drive"
 else
-	echo "Volume $drive not mounted, skipping backup."
+	echo "Volume '$drive' not mounted, skipping backup."
 fi
